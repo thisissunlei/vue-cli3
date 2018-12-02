1e5c92aadb4fde4918c67cc63297ba3ef2650d56
wanglele <wangleping16@163.com>
2018-12-02 15:32:21 +0800

    feat: plugin别名

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import http from '@/plugins/http/index';
+import http from 'plugins/http/index';
 
 // import iView from 'iview';
 import 'iview/dist/styles/iview.css';
