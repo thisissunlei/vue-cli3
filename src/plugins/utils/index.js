e4c244cadd19c2eaa5a59ddc3313a24590ef545b
wanglele <wangleping16@163.com>
2018-12-04 16:24:18 +0800

    feat: 添加公用方法

--- src/plugins/utils/index.js
+++ src/plugins/utils/index.js
@@ -1,7 +1,13 @@
 import dataType from './dataType';
+import thousand from './thousand';
+import throttle from './throttle';
+import dateCompatible from './dateCompatible';
 
 export default {
-  dataType
+  dataType,
+  thousand,
+  throttle,
+  dateCompatible
 }
 
 
