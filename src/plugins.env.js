f2fa4f7df58deac0d7e858a639df46d50836cf4d
wanglele <wangleping16@163.com>
2018-12-13 14:42:48 +0800

    feat: 添加百度地图

--- src/plugins.env.js
+++ src/plugins.env.js
@@ -49,7 +49,12 @@ const jsFiles = [
   'https://web.krspace.cn/kr-op/go/1.8.14/go.js',
   'https://web.krspace.cn/plugins/watermark.js',
   ' https://web.krspace.cn/kr-op/gt/1.0.0/gt.js ',
+  // 百度地图
+  'https://api.map.baidu.com/api?v=2.0&ak=FqwyBT0DR8BunOKYZ0ABDoWU',
+  'https://web.krspace.cn/kr-op/baiduMap/bmap.min.js',
+
   // 私有
+  // 高德地图
   'https://cache.amap.com/lbs/static/es5.min.js',
   'https://webapi.amap.com/maps?v=1.4.9&key=392b034f6d43dfa089a147a1282468d5&plugin=AMap.Geocoder,AMap.MarkerClusterer',
   'https://webapi.amap.com/ui/1.0/main.js'
